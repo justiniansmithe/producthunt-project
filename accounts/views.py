8a2add5c6d4e99e96e5e7cfa192a22a50a645ef6
Justino <justinaverysmith@gmail.com>
2019-10-25 05:28:36 -0500

    fixed site

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
-# Create your views here.
-def home(request):
-	return render(request, 'base.html')
 
 def signup(request):
 	if request.method == 'POST':
